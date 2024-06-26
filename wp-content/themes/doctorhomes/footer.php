032c9000daba34d02d7f502d735e425dac6867de
ZoomREI <dev@zoomrei.com>
2024-06-26 20:57:29 +0300

    Added some responsiveness to the blocks and theme web design

--- wp-content/themes/doctorhomes/footer.php
+++ wp-content/themes/doctorhomes/footer.php
@@ -1,14 +1,14 @@
 <footer id="doctor-homes-footer">
-    <a href="#top" class="back-to-top">
-        <div class="back-to-top-icon">
-            <img src="<?php echo wp_get_attachment_url(150); ?>" alt="">
-        </div>
-        <span>Back to top</span>
-    </a>
     <div class="footer-content">
         <div class="footer-logo">
             <img src="<?php echo wp_get_attachment_url(23); ?>" alt="Site Logo" class="logo-img">
         </div>
+        <a href="#top" class="back-to-top">
+            <div class="back-to-top-icon">
+                <img src="<?php echo wp_get_attachment_url(150); ?>" alt="">
+            </div>
+            <span>Back to top</span>
+        </a>
         <div class="footer-menus">
             <div class="footer-menu">
                 <span class="footer-title">Company</span>
@@ -55,12 +55,9 @@
                 <p class="contact-item"><img src="<?php echo wp_get_attachment_url(22); ?>" alt="Phone Icon" class="contact-icon"> +1 386-688-3295</p>
                 <p class="contact-item"><img src="<?php echo wp_get_attachment_url(21); ?>" alt="Email Icon" class="contact-icon"> Ourstudio@hello.com</p>
             </div>
-            <div class="footer-contact">
-                <span class="footer-title">Contact Us</span>
-                <div class="social-icons">
-                    <a href="#"><img src="<?php echo wp_get_attachment_url(19); ?>" alt="Facebook Icon" class="social-icon"></a>
-                    <a href="#"><img src="<?php echo wp_get_attachment_url(20); ?>" alt="YouTube Icon" class="social-icon"></a>
-                </div>
+            <div class="social-icons">
+                <a href="#"><img src="<?php echo wp_get_attachment_url(19); ?>" alt="Facebook Icon" class="social-icon"></a>
+                <a href="#"><img src="<?php echo wp_get_attachment_url(20); ?>" alt="YouTube Icon" class="social-icon"></a>
             </div>
         </div>
     </div>
@@ -69,8 +66,6 @@
         <p>The content provided on this website is intended for informational purposes only and does not constitute an offer to buy or a solicitation to sell property. All representations regarding potential transactions are preliminary and subject to change. No content on this site should be interpreted as a binding promise or guarantee of a specific outcome. Each property purchase will be subject to individual negotiation and the execution of a definitive agreement, the terms of which may vary. We make no warranties regarding the accuracy, completeness, legality, or reliability of any information offered on this website. Sellers are advised to conduct their own due diligence and consult with professional advisors prior to entering into any transaction with us.</p>
     </div>
 </footer>
-
-
 
 <?php wp_footer(); ?>
 </body>
