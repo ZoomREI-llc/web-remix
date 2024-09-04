52ffc50f64ff84b75e28b6b0e10734f5d80d9c92
ZoomREI <dev@zoomrei.com>
2024-09-04 16:17:55 +0300

    removed wp themes and updated header.php to contain <title>

--- wp-content/themes/doctorhomes/templates/header.php
+++ wp-content/themes/doctorhomes/templates/header.php
@@ -18,6 +18,7 @@
 
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(''); ?></title>
     <?php wp_head(); ?>
 </head>
 
