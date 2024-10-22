2ce1e49f50a8463b453547681f7394e6bb8393cf
Mishaka40 <mishaka40@gmail.com>
2024-10-22 18:35:35 +0300

    Added s2-form

---
+++ wp-content/plugins/doctor-homes-blocks/build/s2-form/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '931b45635e4985da499c');
---
+++ wp-content/plugins/doctor-homes-blocks/src/s2-form/render.php
@@ -0,0 +1,65 @@
+
+<div class="s2-form">
+    <div class="s2-form__container">
+        <div data-page="main" class="s2-form__step">
+            <div class="s2-form__progress" style="--progress: 50%">
+                <span class="done"></span>
+                <span class="process"></span>
+                <span ></span>
+            </div>
+            <div class="s2-form__title">
+                <h1>Thanks for reaching out to us! </h1>
+            </div>
+            <div class="s2-form__subtitle">
+                <p>You’re just a few steps away from getting a fast cash offer for your house...</p>
+            </div>
+            <div class="s2-form__title big">
+                <p>Why should you choose us?</p>
+            </div>
+            <div class="s2-form__subtitle">
+                <p>Watch a short video and find out!</p>
+            </div>
+            <div class="s2-form__video">
+                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1012736746?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Chris Buys Homes Commercial"></iframe></div>
+                <script src="https://player.vimeo.com/api/player.js"></script>
+            </div>
+            <button data-page-link="form" type="button" class="s2-form__btn">
+                Get My Offer
+                <svg width="23" height="19" viewBox="0 0 23 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.7776 2.31513L12.386 3.74424L12.3862 3.74444L12.3871 3.7453L12.3907 3.74882L12.4053 3.763L12.4625 3.81883L12.6808 4.03189C12.869 4.2158 13.1381 4.47907 13.4611 4.79604C14.1074 5.43031 14.968 6.27812 15.8277 7.1347C15.9415 7.24812 16.0552 7.36154 16.1682 7.4745H2.30273C1.19816 7.4745 0.302734 8.36993 0.302734 9.4745C0.302734 10.5791 1.19816 11.4745 2.30273 11.4745H15.9375C15.9009 11.511 15.8643 11.5475 15.8277 11.584C14.968 12.4406 14.1074 13.2884 13.4611 13.9227C13.1381 14.2396 12.869 14.5029 12.6808 14.6868L12.4625 14.8999L12.4053 14.9557L12.3907 14.9699L12.3871 14.9734L12.3862 14.9743L12.386 14.9745L13.7813 16.4074L12.386 14.9745C11.5946 15.7451 11.5778 17.0114 12.3484 17.8027C13.1191 18.594 14.3853 18.6108 15.1766 17.8402L13.7813 16.4074C15.1766 17.8402 15.1767 17.8402 15.1767 17.8401L15.177 17.8398L15.178 17.8389L15.1819 17.8351L15.197 17.8204L15.2554 17.7634L15.4762 17.5479C15.6662 17.3623 15.9374 17.0969 16.2628 16.7776C16.9132 16.1393 17.7817 15.2838 18.6511 14.4175C19.5182 13.5534 20.3954 12.6696 21.0596 11.9777C21.3891 11.6345 21.6822 11.3218 21.8993 11.076C22.0043 10.9571 22.1162 10.8261 22.211 10.7017C22.2553 10.6434 22.326 10.5476 22.3949 10.4322C22.4288 10.3755 22.4851 10.2761 22.5385 10.148C22.5756 10.0591 22.6974 9.76086 22.6974 9.35934C22.6974 8.95781 22.5756 8.65954 22.5385 8.57063C22.4851 8.44253 22.4288 8.3432 22.3949 8.2865C22.326 8.17109 22.2553 8.07522 22.211 8.01695C22.1162 7.89256 22.0043 7.76156 21.8993 7.64264C21.6822 7.39682 21.3891 7.0842 21.0596 6.74099C20.3954 6.0491 19.5182 5.16526 18.651 4.30122C17.7817 3.43492 16.9132 2.57943 16.2628 1.94111C15.9374 1.62178 15.6662 1.35644 15.4762 1.1708L15.2554 0.955279L15.197 0.898338L15.1819 0.883625L15.178 0.87984L15.177 0.878856L15.1767 0.878594C15.1767 0.878529 15.1766 0.878488 13.9018 2.18758L15.1766 0.878485C14.3853 0.10787 13.119 0.124675 12.3484 0.91602C11.5778 1.70736 11.5946 2.97358 12.386 3.7442L13.7776 2.31513Z" fill="white" />
+                </svg>
+            </button>
+        </div>
+        <div data-page="form" class="s2-form__step">
+            <div class="s2-form__progress" style="--progress: 50%">
+                <span class="done"></span>
+                <span class="done"></span>
+                <span class="process"></span>
+            </div>
+            <div class="s2-form__title">
+                <h2>Let Us Prepare the Best Offer for You</h2>
+            </div>
+            <div class="s2-form__subtitle">
+                <p>We just need a little more information to understand your situation and offer you the best options, including an instant cash offer.
+					<br>
+					<br>
+					If you’re unsure about any details, feel free to leave those fields blank for now.
+				</p>
+            </div>
+            <div class="s2-form__form">
+                <?php echo $content; ?>
+            </div>
+        </div>
+		<div data-page="thanks" class="s2-form__step">
+			<div class="s2-form__progress" style="--progress: 100%">
+				<span class="done"></span>
+				<span class="done"></span>
+				<span class="done"></span>
+			</div>
+			<div class="s2-form__thanks">
+				<h3>Thank you!</h3>
+				<p>We've received your information and will be in touch soon with your offer.</p>
+			</div>
+		</div>
+    </div>
+</div>
