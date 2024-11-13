53df58c37597806665739a37e5911153283917e0
Mishaka40 <mishaka40@gmail.com>
2024-11-13 19:39:37 +0200

    Replaced testimonials sliders with swiper.js

--- wp-content/plugins/doctor-homes-blocks/src/sell-fast-testimonials/view.js
+++ wp-content/plugins/doctor-homes-blocks/src/sell-fast-testimonials/view.js
@@ -1,169 +1,53 @@
-// import gsap from "gsap";
-// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-//
 
 function sfVirtueCarouselCallback() {
-	gsap.registerPlugin(ScrollToPlugin);
+	let sections = document.querySelectorAll('.testimonial-carousel-wrapper')
 
-	// Function to handle the carousel logic for smaller screens
-	const initializeCarousel = () => {
-		const track = document.querySelector(".carousel-container");
-		const slides = Array.from(track.children);
-		const dotsNavs = document.querySelectorAll(".carousel-dots");
-		const prevBtns = document.querySelectorAll(".carousel-prev");
-		const nextBtns = document.querySelectorAll(".carousel-next");
-
-		let currentSlideIndex = 0;
-		let isScrolling = false; // Flag to prevent double-triggering
-
-		track.scrollTo(0, 0)
-		// Function to manually calculate slide positions and handle resize
-		const setSlidePositions = () => {
-			updateDots(-1, currentSlideIndex); // Initialize dots correctly
-		};
-
-		slides[0].classList.add('active')
-		const updateDots = (currentIndex, targetIndex) => {
-			dotsNavs.forEach(function (dotsNav) {
-				let dots = Array.from(dotsNav.children);
-
-				if (dots[targetIndex]) {
-					dots[currentIndex]?.classList.remove("active");
-					dots[targetIndex].classList.add("active");
+	sections.forEach(function (section) {
+		let sliderEl = section.querySelector('.swiper')
+		let prevButtons = section.querySelectorAll('.js-swiper-prev')
+		let nextButtons = section.querySelectorAll('.js-swiper-next')
+		let pagination = section.querySelector('.swiper-pagination')
+		let swiper = new Swiper(sliderEl, {
+			slidesPerView: 'auto',
+			spaceBetween: 48,
+			autoHeight: true,
+			speed: 600,
+			pagination: {
+				el: pagination,
+				clickable: true
+			},
+			on: {
+				slideChange: function () {
+					updateButtons();
 				}
-			})
-		};
-
-		const moveToSlide = (targetIndex) => {
-			const slideWidth = slides[0].getBoundingClientRect().width;
-			const targetSlide = slides[targetIndex];
-			const targetPosition =
-				targetSlide.offsetLeft -
-				(track.parentElement.getBoundingClientRect().width - slideWidth) / 2;
-
-			isScrolling = true; // Set flag to prevent scroll-triggered updates
-
-			if(targetIndex === 0){
-				prevBtns.forEach(function (prevBtn) {
-					prevBtn.disabled = true
-				})
-			} else {
-				prevBtns.forEach(function (prevBtn) {
-					prevBtn.disabled = false
-				})
+			},
+			breakpoints: {
+				1024: {
+					autoHeight: false
+				}
 			}
-			if(targetIndex < slides.length - 1){
-				nextBtns.forEach(function (nextBtn) {
-					nextBtn.disabled = false
-				})
-			} else {
-				nextBtns.forEach(function (nextBtn) {
-					nextBtn.disabled = true
-				})
-			}
-			track.style.overflowX = 'hidden'
-			gsap.to(track, {
-				scrollTo: { x: targetPosition },
-				duration: 0.6,
-				ease: "power2.inOut",
-				onComplete: () => {
-					setTimeout(function () {
-						track.style.overflowX = 'auto'
-						isScrolling = false; // Reset flag after animation completes
-					}, 50)
-				},
-			});
-			updateDots(currentSlideIndex, targetIndex);
-			currentSlideIndex = targetIndex;
-			slides.forEach(item=>item.classList.remove('active'))
-			targetSlide.classList.add('active')
-		};
-
-
-		// Event listener for dot navigation
-
-		dotsNavs.forEach(function (dotsNav) {
-			dotsNav.addEventListener("click", (e) => {
-				const targetDot = e.target.closest("span");
-				if (!targetDot) return;
-
-				let dots = Array.from(dotsNav.children);
-
-				const targetIndex = dots.findIndex((dot) => dot === targetDot);
-				moveToSlide(targetIndex);
-			});
-		})
-		prevBtns.forEach(function (prevBtn) {
-			prevBtn.addEventListener("click", (e) => {
-				if(currentSlideIndex - 1 >= 0) {
-					moveToSlide(currentSlideIndex - 1);
-				}
-			});
-		})
-		nextBtns.forEach(function (nextBtn) {
-			nextBtn.addEventListener("click", (e) => {
-				if (currentSlideIndex + 1 < slides.length) {
-					moveToSlide(currentSlideIndex + 1);
-				}
-			});
 		})
 
-		let waitForScrollEnd = 0
-
-		function onScroll() {
-			if (isScrolling) return; // Prevents handling the event during slide transition
-
-			clearTimeout(waitForScrollEnd)
-			waitForScrollEnd = setTimeout(function () {
-				const slideWidth = slides[0].offsetWidth;
-				const scrolledIndex = Math.round(track.scrollLeft / slideWidth);
-				if (
-					scrolledIndex < slides.length
-				) {
-					updateDots(currentSlideIndex, scrolledIndex);
-					currentSlideIndex = scrolledIndex;
-					moveToSlide(currentSlideIndex)
-				}
-			}, 15)
+		function updateButtons() {
+			prevButtons.forEach(btn=>btn.disabled = swiper.isBeginning)
+			nextButtons.forEach(btn=>btn.disabled = swiper.isEnd)
 		}
 
-		// Sync dots based on scroll position
-		track.addEventListener("touchstart", () => {
-			isScrolling = true
-		})
-		track.addEventListener("touchend", () => {
-			isScrolling = false
-			onScroll()
-		})
-		track.addEventListener("scroll", () => {
-			onScroll()
-		});
-
-		// Set the initial active dot
-		updateDots(-1, 0);
-
-		// Set up slide positions on load and resize
-		setSlidePositions();
-		window.addEventListener("resize", setSlidePositions);
-	};
-
-	// Initialize the carousel logic on page load
-	initializeCarousel();
-
-	// Re-check and reinitialize the carousel logic on window resize
-	window.addEventListener("resize", () => {
-		// Remove any previously added event listeners to avoid duplication
-		const track = document.querySelector(".carousel-track");
-		track.removeAttribute("style"); // Reset inline styles added previously
-
-		// Reinitialize if the condition is met
-		initializeCarousel();
-	});
+		updateButtons()
+		prevButtons.forEach(btn=>btn.addEventListener('click', function (e) {
+			e.preventDefault()
+			swiper.slidePrev()
+		}))
+		nextButtons.forEach(btn=>btn.addEventListener('click', function (e) {
+			e.preventDefault()
+			swiper.slideNext()
+		}))
+	})
 }
 
 document.addEventListener("DOMContentLoaded", function () {
 	loadScript([
-		'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js',
-		'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollToPlugin.min.js'
+		'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+		'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css'
 	], sfVirtueCarouselCallback)
 });
