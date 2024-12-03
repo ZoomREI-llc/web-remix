43d6f860813ac67255a7214b09ec93bbc1091b12
Mishaka40 <mishaka40@gmail.com>
2024-12-03 10:19:31 +0200

    Added lead form v2

--- wp-content/plugins/doctor-homes-blocks/src/lead-form/view.js
+++ wp-content/plugins/doctor-homes-blocks/src/lead-form/view.js
@@ -1,159 +1,103 @@
-import { fadeIn, fadeOut, slideUp, slideDown } from "./modules/helpers";
+import { fadeIn, fadeOut, slideUp, slideDown, sessionStorageUTM, trigger } from "./modules/helpers";
+import { populateUtms, getRedirectParams } from "./modules/form_helpers";
 import { validate } from "./modules/validate";
 import { telInputMask } from "./modules/telInputMask";
 import { inputSelect } from "./modules/inputSelect";
 
 function leadFormCallback() {
-    let leadForm = document.getElementById('dh-lead-form')
-    if(!leadForm){
+    let leadForms = document.querySelectorAll('.lead-form:not(.is-initialized)')
+    if(!leadForms.length){
         return;
     }
-    let phoneInput = leadForm.querySelector('[data-validation="tel-mask"]')
+    leadForms.forEach(function (leadForm) {
+        let leadFormConfig = JSON.parse(document.getElementById('form-config-'+leadForm.id).innerHTML)
+        let phoneInput = leadForm.querySelector('[data-validation="tel-mask"]')
+        let formName = leadForm.closest('[data-form-name]') ? leadForm.closest('[data-form-name]').dataset.formName : leadForm.name
 
-    function populateUtms(formData) {
-        let utms = {
-            'utm_source': 'get',
-            'utm_keyword': 'get',
-            'utm_term': 'get',
-            'utm_device': 'get',
-            'utm_gclid': 'get',
-            'utm_campaign': 'get',
-            'utm_medium': 'get',
-            'utm_content': 'get',
-            'fbclid': 'get',
-            'gclid': 'get',
-            'page_url': ()=>{
-                return document.location.href
-            },
-            'lead_source': ()=>{
-                return '??'
-            },
-            'timestamp': ()=>{
-                return (new Date()).getTime()
-            },
-            'client_id': ()=>{
-                return '??'
-            },
-            'session_id': ()=>{
-                return '??'
-            },
-            'form_name': leadForm.name,
-        }
-        let getParams = new URLSearchParams(window.location.search)
+        leadForm.classList.add('is-initialized')
 
-        Object.keys(utms).forEach(function (utmName) {
-            if(utms[utmName] === 'get'){
-                if(getParams.get(utmName)) {
-                    formData.set(utmName, getParams.get(utmName))
+        function sendAjax() {
+            let xhr = new XMLHttpRequest();
+            let formData = populateUtms(leadForm, new FormData(leadForm));
+            let formBtn = leadForm.querySelector('[type="submit"]');
+
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                event: "submitStep1",
+                formName: formName,
+                fullName: formData.get('fullName'),
+                street: formData.get('street'),
+                city: formData.get('city'),
+                state: formData.get('state'),
+                country: "US",
+                zipcode: formData.get('zipcode'),
+                email: formData.get('email'),
+                phone: formData.get('phone')
+            });
+            trigger(leadForm, 'lead-form-submit')
+
+            if (formBtn) {
+                formBtn.classList.add('is-loading');
+                formBtn.setAttribute('disabled', 'disabled');
+            }
+
+            formData.append('webhooks', JSON.stringify(leadFormConfig.webhooks))
+            xhr.open('post', leadFormConfig.handler);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    if(leadFormConfig.redirect) {
+                        let redirectParams = getRedirectParams(formData, leadFormConfig.query)
+
+                        document.location.href = leadFormConfig.redirect + (redirectParams ? '?'+redirectParams : '')
+                    }
                 }
-            } else if(typeof utms[utmName] === 'function'){
-                formData.set(utmName, utms[utmName]())
-            } else {
-                formData.set(utmName, utms[utmName])
+            };
+            xhr.send(formData);
+        }
+        function initAddress() {
+            let addressInput = leadForm.querySelector('[data-validation="address-autocomplete"]')
+            let addressInputBtn = leadForm.querySelector('[type="submit"]')
+            let autocomplete = new google.maps.places.Autocomplete(addressInput, {
+                types: ["address"],
+                componentRestrictions: { country: "us" },
+            });
+            let inputStreet = leadForm.querySelector('[name="street"]')
+            let inputCity = leadForm.querySelector('[name="city"]')
+            let inputState = leadForm.querySelector('[name="state"]')
+            let inputZipcode = leadForm.querySelector('[name="zipcode"]')
+
+            addressInput.autocompleteInstance = autocomplete
+            autocomplete.addListener("place_changed", function () {
+                if(addressInput.isValid()){
+                    inputStreet.value = addressInput.dataset.street
+                    inputCity.value = addressInput.dataset.city
+                    inputState.value = addressInput.dataset.state
+                    inputZipcode.value = addressInput.dataset.zipcode
+                }
+                trigger(leadForm, 'lead-form-interaction')
+            });
+            if(addressInputBtn) {
+                addressInputBtn.addEventListener('click', function (e) {
+                    addressInput.closest('.input').classList.remove('is-error')
+                })
             }
+        }
+
+        telInputMask(phoneInput, {
+            mask: '(xxx) xxx - xxxx',
+            hiddenInput: true
         })
-
-        return formData;
-    }
-    function getRedirectParams(formData) {
-        let params = {
-            'fullName': 'full-name',
-            'phone': 'phone',
-            'email': 'email',
-            'propertyAddress': 'propaddress',
-            'street': 'propstreet',
-            'city': 'propcity',
-            'state': 'propstate',
-            'zipcode': 'propzip',
-            'country': 'propcountry',
-        }
-        let query = {}
-
-        Object.keys(params).forEach(function (inputName) {
-            let propName = params[inputName]
-
-            if(inputName === 'country'){
-                query[propName] = 'United States'
-            } else if(formData.get(inputName)) {
-                query[propName] = formData.get(inputName)
-            }
+        validate(leadForm, {
+            submitFunction: sendAjax,
+            trackErrors: true
         })
-
-        return new URLSearchParams(query).toString()
-    }
-    function sendAjax() {
-        let formData = populateUtms(new FormData(leadForm));
-        let formBtn = leadForm.querySelector('[type="submit"]');
-        let xhr = new XMLHttpRequest();
-
-        if (formBtn) {
-            formBtn.setAttribute('disabled', 'disabled');
-        }
-
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-            event: "lead_step_1",
-            formName: leadForm.name,
-            fullName: formData.get('fullName'),
-            street: formData.get('street'),
-            city: formData.get('city'),
-            state: formData.get('state'),
-            country: "US",
-            zipcode: formData.get('zipcode'),
-            email: formData.get('email'),
-            phone: formData.get('phone')
-        });
-
-        xhr.open(leadForm.method, leadForm.action);
-        xhr.send(formData);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                document.location.href = leadForm.dataset.redirect + '?' + getRedirectParams(formData)
-            }
-        };
-    }
-    function initAddress() {
-        let addressInput = leadForm.querySelector('[data-validation="address-autocomplete"]')
-        let addressInputBtn = leadForm.querySelector('[type="submit"]')
-        let nextStep = leadForm.querySelector('.lead-form__fields')
-        let autocomplete = new google.maps.places.Autocomplete(addressInput, {
-            types: ["address"],
-            componentRestrictions: { country: "us" },
-        });
-        let inputStreet = leadForm.querySelector('[name="street"]')
-        let inputCity = leadForm.querySelector('[name="city"]')
-        let inputState = leadForm.querySelector('[name="state"]')
-        let inputZipcode = leadForm.querySelector('[name="zipcode"]')
-
-        addressInput.autocompleteInstance = autocomplete
-        autocomplete.addListener("place_changed", function () {
-            if(addressInput.isValid()){
-                inputStreet.value = addressInput.dataset.street
-                inputCity.value = addressInput.dataset.city
-                inputState.value = addressInput.dataset.state
-                inputZipcode.value = addressInput.dataset.zipcode
-            }
-        });
-        if(addressInputBtn) {
-            addressInputBtn.addEventListener('click', function (e) {
-                addressInput.closest('.input').classList.remove('is-error')
-            })
-        }
-    }
-
-    telInputMask(phoneInput, {
-        mask: '(xxx) xxx - xxxx',
-        hiddenInput: true
+        initAddress()
+        inputSelect()
     })
-    validate(leadForm, {
-        submitFunction: sendAjax,
-        trackErrors: true
-    })
-    initAddress()
-    inputSelect()
 }
 
-window.leadFormCallback = leadFormCallback
 document.addEventListener("DOMContentLoaded", function () {
+    sessionStorageUTM()
+
     loadScript(`https://maps.googleapis.com/maps/api/js?key=${formConfig.googleMapsApiKey}&libraries=places`, leadFormCallback);
 });
